a07715f3d34856db7bcf3ebbbf3b50ec0c0bab8a
Woodiik <vovikvova04@gmail.com>
2023-01-28 16:39:16 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,39 @@
 import throttle from "lodash.throttle";
 
 const formEl = document.querySelector('.feedback-form');
-formData = {}
-const localStorageKey = "feedback-form-state"
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+formData = {};
+const localStorageKey = "feedback-form-state";
 
-fillForm()
+fillForm();
 
 
 
 formEl.addEventListener('input', throttle(e => {
    formData[e.target.name] = e.target.value
    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-}, 500))
+}, 500));
+
 
 formEl.addEventListener('submit', e => {
-   e.preventDefault();
    localStorage.removeItem(localStorageKey);
-   e.currentTarget.reset()
-   console.log(formData)
+   if (inputEl.value === '' || textareaEl.value === '') {
+      alert('Заповніть усі поля форми')
+   } else {
+   e.preventDefault();
+   e.currentTarget.reset();
+   console.log(formData);}
+  
 });
+
+
 
 
 function fillForm() {
    const savedData = JSON.parse(localStorage.getItem(localStorageKey));
    if (savedData) {
-      document.querySelector('input').value = savedData.email
-      document.querySelector('textarea').value = savedData.message
+      inputEl.value = savedData.email
+      textareaEl.value = savedData.message
    }
 };
